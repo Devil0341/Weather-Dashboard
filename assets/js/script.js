046cdfdc85e1d24593765f53a96a104f2edcdc4a
Sureshot0341 <gregpetropoulos@yahoo.com>
2021-03-02 22:35:53 -0500

    90% finished with html and css, already have a functional search button in js, need to work heavily on the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,47 @@
 //search button
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-});
+var searchButtonEl= $('.btn')
+var searchInputEl = $('#city-list')
+
+searchButtonEl.on('click', function(event){
+  event.preventDefault()
+  // grabbing the city
+  var inputCity = $('#form1').val();
+
+
+  //if nothing entered and button is clicked
+if (!inputCity){
+  console.log('No City filled out');
+  return;
+}
+
+ var cityHistoryEl = $('<li class="list-group-item list-group-item-dark col-12 m-1">')
+
+
+// $('<li class="flex-row justify-space-between align-center p-2 bg-light text-dark">');
+
+cityHistoryEl.text(inputCity);
+
+//Print to page lecture 5-6
+searchInputEl.append(cityHistoryEl)
+
+
+})
+
+
+
+// api from weather website
+// api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={API key}
+
+// //fetch data 
+// // Replace ./data.json with your JSON feed
+// fetch('./data.json')
+//   .then((response) => {
+//     return response.json()
+//   })
+//   .then((data) => {
+//     // Work with JSON data here
+//     console.log(data)
+//   })
+//   .catch((err) => {
+//     // Do something for an error here
+//   })
